202e79d91b93293286bf0d2b0dcd05a8a2670adf
Varsha Balaji <vrshbalaji@gmail.com>
2026-04-08 22:23:56 +0000

    play done

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // add javascript here
-let guess = 0;
 let answer = 0;
 let guessCount = 0;
-let totalWins = 0;
 const scores = [];
 
 document.getElementById("playBtn").addEventListener("click", play);
+document.getElementById("guessBtn").addEventListener("click", makeGuess);
 
 function play(){
     let range = 0;
@@ -25,3 +24,50 @@ function play(){
     playBtn.disabled = true;
 }
 
+function makeGuess(){
+    let guess = parseInt(document.getElementById("guess").value);
+    if(isNaN(guess)){
+        msg.textContent = "Please enter a valid number";
+        return;
+    }
+    guessCount++;
+    if(guess == answer){
+        msg.textContent = "Correct! It took " + guessCount + " tries.";
+        updateScore(guessCount);
+        resetGame();
+    }
+    else if(guess < answer){
+        msg.textContent = "Too low, try again.";
+    } else{
+        msg.textContent = "Too high, try again.";
+    }
+    
+}
+
+function updateScore(score){
+    scores.push(score);
+    wins.textContent = "Total wins: " + scores.length;
+    let sum = 0;
+    for(let i = 0; i < scores.length; i++){
+        sum += scores[i];
+    }
+    avgScore.textContent = "Average Score: " + (sum / scores.length).toFixed(1);
+
+    scores.sort(function(a,b){return a-b;}); //sort score increasing
+
+    let lb = document.getElementsByName("leaderboard");
+    for(let i = 0; i < lb.length; i++){
+        if(i < scores.length){
+            lb[i].textContent = scores[i];
+        }
+    }
+}
+function resetGame(){
+    guess.value = "";
+    guessBtn.disabled = true;
+    giveUpBtn.disabled = true;
+    playBtn.disabled = false;
+    e.disabled = false;
+    m.disabled = false;
+    h.disabled = false;
+}
